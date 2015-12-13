c96e449c1ea9d28b82310d543fa9ac2466e56fca
Ewen Fagon <ewen.bzh@live.fr>
2015-12-13 15:19:20 +0100

    Joints attached to the ground are displayed accordingly. Solid lines are thicker

--- src/Line.java
+++ src/Line.java
@@ -3,6 +3,7 @@ import java.awt.*;
 public class Line extends Solid {
 	public double m_length;
 	public int m_direction;
+	public final int WIDTH_LINE = 2;
 
 	//Constructor
 	public Line(Joint joint, double length) {
@@ -11,10 +12,51 @@ public class Line extends Solid {
 		m_length = length;
 	}
 
-	public void draw(Graphics g) {
+	public Point[] getRectangle () {
 		double rot = getAbsoluteRotation();
 		Point a = getAbsolutePosition();
-		g.drawLine((int)a.m_x, (int)a.m_y, (int)(a.m_x + m_direction * m_length * Math.cos(rot)), (int)(a.m_y + m_direction * m_length * Math.sin(rot)));
+		Point topLeft = new Point (
+		(int)(a.m_x + (-1) * m_direction * WIDTH_LINE * Math.cos(rot+Math.PI/2)),
+		(int)(a.m_y + (-1) * m_direction * WIDTH_LINE * Math.sin(rot+Math.PI/2)));
+
+		Point botLeft = new Point (
+		(int)(a.m_x + m_direction * WIDTH_LINE * Math.cos(rot+Math.PI/2)),
+		(int)(a.m_y + m_direction * WIDTH_LINE * Math.sin(rot+Math.PI/2)));
+
+		Point topRight = new Point (
+		(int) ((a.m_x + m_direction * m_length * Math.cos(rot)) + (-1) * m_direction * WIDTH_LINE * Math.cos(rot+Math.PI/2)),
+		(int) ((a.m_y + m_direction * m_length * Math.sin(rot)) + (-1) * m_direction * WIDTH_LINE * Math.sin(rot+Math.PI/2)));
+
+		Point botRight = new Point (
+		(int) ((a.m_x + m_direction * m_length * Math.cos(rot)) + m_direction * WIDTH_LINE * Math.cos(rot+Math.PI/2)),
+		(int) ((a.m_y + m_direction * m_length * Math.sin(rot)) + m_direction * WIDTH_LINE * Math.sin(rot+Math.PI/2)));
+
+		Point [] pointsRectangle = new Point [4];
+		pointsRectangle [0] = topLeft;
+		pointsRectangle [1] = botLeft;
+		pointsRectangle [2] = botRight;
+		pointsRectangle [3] = topRight;
+		return pointsRectangle;
+	}
+
+	public int[] getXCoord (Point[] points) {
+		int [] xCoord = new int[4];
+		for (int i = 0; i<4; i++) {
+			xCoord[i]=points[i].m_x;
+		}
+		return xCoord;
+	}
+
+	public int[] getYCoord (Point[] points) {
+		int [] yCoord = new int[4];
+		for (int i = 0; i<4; i++) {
+			yCoord[i]=points[i].m_y;
+		}
+		return yCoord;
+	}
+
+	public void draw(Graphics g) {
+		g.fillPolygon(getXCoord(getRectangle()), getYCoord(getRectangle()), 4);
 	}
 
 	public Point getClosePoint(Point p, double snapping_distance) {
--- src/Revolute.java
+++ src/Revolute.java
@@ -10,6 +10,15 @@ public class Revolute extends Joint {
 
     public void draw(Graphics g) {
         Point absPos = getAbsolutePosition();
-        g.drawOval(absPos.m_x - 5, absPos.m_y - 5, 10, 10);
+        if (m_s1.m_isGround) {
+            g.drawOval(absPos.m_x - 5, absPos.m_y - 5, 10, 10);
+            g.drawLine(absPos.m_x, absPos.m_y+5, absPos.m_x, absPos.m_y+20);
+            g.drawLine(absPos.m_x-10, absPos.m_y+20, absPos.m_x+10, absPos.m_y+20);
+            for (int i = -15; i<=5; i=i+5) {
+                g.drawLine(absPos.m_x+i, absPos.m_y+25, absPos.m_x+i+5, absPos.m_y+20);
+            }
+        } else {
+            g.drawOval(absPos.m_x - 5, absPos.m_y - 5, 10, 10);
+        }
     }
 }
